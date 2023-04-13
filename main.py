f6bff025a957aacd1e1e38cbc5b36998bdb162a0
John Victor <37747572+johnvictorfs@users.noreply.github.com>
2023-04-12 22:33:37 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def organize_folders():
+    print('Organized stuff')
+
+if __name__ == '__main__':
+    organize_folders()
+
---
+++ tests/test_organization.py
@@ -0,0 +1,13 @@
+disorganized_folders = [
+    "Evangelion E1",
+    "Evangelion E3",
+    "Kimetsu no Yaiba S1E1",
+    "Kimetsu no Yaiba S1E3",
+    "Kimetsu no Yaiba S2E2",
+    "Kimetsu no Yaiba Season 2x4",
+    "Evangelion E2",
+    "Evangelion E4",
+    "Kimetsu no Yaiba S1E2",
+    "Kimetsu no Yaiba S2E1",
+    "Kimetsu no Yaiba S2x3",
+]
