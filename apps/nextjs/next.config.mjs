bd416d372ca3c63e267fe7bbbfafc4e11d754879
johnvictorfs <37747572+johnvictorfs@users.noreply.github.com>
2023-04-16 23:02:32 -0300

    Fix linting issues

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -8,10 +8,10 @@
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/db"],
+  transpilePackages: ['@acme/api', '@acme/db'],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
-};
+}
 
-export default config;
+export default config
